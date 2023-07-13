181b1824f037f1ba8d0fb216f6e9fd1f54124aab
github.Nourafniputri <email.nourafni24@gmail.com>
2023-07-13 21:09:52 +0700

    boilerplate serverside

---
+++ server/models/history.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config');
+
+class History {
+    #model = sequelize.define('history', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        userSkor: {
+            type: DataTypes.INTEGER
+        },
+        userId: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        createdAt: {
+            allowNull: false,
+            defaultValue: DataTypes.literal('NOW()'),
+            type: DataTypes.DATE
+        }
+    })
+};
+
+const user = new User()
+module.exports = { user }
---
+++ server/sql/create_tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE public."user" (
+	id serial NOT NULL,
+	username varchar NULL,
+	"password" varchar NULL,
+	email varchar NULL,
+	created_at timestamptz NOT NULL DEFAULT NOW(),
+	updated_at timestamptz NOT NULL DEFAULT NOW(),
+	CONSTRAINT user_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE public.biodata_user (
+	id serial NOT NULL,
+	umur integer NULL,
+	city varchar NULL,
+	country varchar NULL,
+	created_at timestamptz NOT NULL DEFAULT NOW(),
+	updated_at timestamptz NOT NULL DEFAULT NOW(),
+	user_id integer NOT NULL,
+	CONSTRAINT biodata_user_pk PRIMARY KEY (id),
+	CONSTRAINT biodata_user_fk FOREIGN KEY (user_id) REFERENCES public."user"(id) ON UPDATE CASCADE
+);
+
+CREATE TABLE public.history (
+	id serial NOT NULL,
+	user_skor integer NULL,
+	created_at timestamptz NOT NULL DEFAULT NOW(),
+	user_id integer NOT NULL,
+	CONSTRAINT history_pk PRIMARY KEY (id),
+	CONSTRAINT history_fk FOREIGN KEY (user_id) REFERENCES public."user"(id) ON UPDATE CASCADE
+);
