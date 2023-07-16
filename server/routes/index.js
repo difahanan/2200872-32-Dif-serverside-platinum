171eb252948094f5497271e254a664a5c4cb2866
Hansen Yudistira <hansencancer27@gmail.com>
2023-07-16 15:40:31 +0700

    API untuk getgamelist

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const { MainController } = require('../controllers/MainController')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/gamelist/get', MainController.getGameList)
 
 module.exports = router;
