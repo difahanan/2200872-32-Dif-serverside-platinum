84ba3ee9ea22f013d2a5766da494db7429e2fe1f
Agnes Septilia <agnes.septilia@gmail.com>
2023-07-20 18:55:54 +0700

    resend Routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,27 +8,28 @@ const { PassportAuthorizationCheck } = require('../lib/PassportAuthorizationChec
 const { LandingPageController } = require('../controllers/LandingPageController');
 
 
-/* GET LANDING PAGE */ 
-router.get('/', PassportMainController.MainPage);
-
 /* REGISTER PAGE */
 router.get('/register', PassportMainController.getRegisterPage)
 router.post('/register', PassportMainController.postRegisterPage)
 
 /* LOGIN PAGE */
 router.get('/login', PassportMainController.getLoginPage);
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
+// router.post('/login', passport.authenticate('local', {
+//     successRedirect: '/dashboard',
+//     failureRedirect: '/login',
+//     failureFlash: true
+// }))
 
-
-/* DASHBOARD PAGE */ 
-router.get('/dashboard', PassportAuthorizationCheck, PassportMainController.getDashboardPage)
-
-/* LOGOUT */
-router.post('/logout', PassportMainController.logout)
+router.post('/login', passport.authenticate('local', 
+    {
+        failureRedirect: '/login',
+        failureFlash: true
+    })
+    , function(req, res) {
+        console.log(req.user)
+        res.status(200).json({message: "success"})
+    }
+)
 
 
 
